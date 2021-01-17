e7e8e8fbf4d3b745922bad7e4f1fc5f0ce7f3647
Meet Patel <meetaiims@yahoo.com>
2021-01-17 04:28:33 -0600

    async doWork() using async-await keywords to capture promises

--- async-await.js
+++ async-await.js
@@ -1,24 +1,57 @@
 function makeRequest(location) {
-    return new Promise((resolve, reject) => {
-        console.log(`Making Request to ${location}`)
-        if (location === 'Google') {
-            resolve('Google says hi')
-        } else {
-            reject('We can only talk to Google')
-        }
-    })
+  return new Promise((resolve, reject) => {
+    console.log(`Making Request to ${location}`);
+    if (location === 'Google') {
+      resolve('Google says hi');
+    } else {
+      reject('We can only talk to Google');
+    }
+  });
 }
 
 function processRequest(response) {
-    return new Promise((resolve, reject) => {
-        console.log('Processing response')
-        resolve(`Extra Information ${response}`)
-    })
+  return new Promise((resolve, reject) => {
+    console.log('Processing response');
+    resolve(`Extra Information + ${response}`);
+  });
 }
 
-console.log(makeRequest('Google'));
-makeRequest('NotGoogle')
-    .then(message => console.log(message))
-    .catch(message => console.log(message))
-console.log(processRequest('RandomWillResolve'));
-console.log(processRequest('ThisAlsoShallResolve'))
+/*
+
+makeRequest('Facebook')
+  .then((res) => {
+    console.log(res);
+    return processRequest(res);
+  })
+  .then((processRes) => {
+    console.log(processRes);
+  }).catch(err => {
+      console.log(err)
+  })
+
+*/
+
+async function doWork() {
+  try {
+    const response = await makeRequest('Google');
+    console.log(response);
+    console.log('Response received');
+    const processResponse = await processRequest(response);
+    console.log(processResponse);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function notAsyncWork() {
+  const response = makeRequest('Google');
+  console.log(response);
+  console.log('Response received');
+  const processResponse = processRequest(response);
+  console.log(processResponse);
+}
+notAsyncWork();
+doWork();
+console.log('Asynchronous code example')
+
+
